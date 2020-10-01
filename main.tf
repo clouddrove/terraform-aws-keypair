08757a855118f822dcc745a52df5452da4ba39bf
himanshu-uniyal <himanshu@clouddrove.com>
2020-10-01 17:54:48 +0530

    added labels

--- main.tf
+++ main.tf
@@ -23,5 +23,5 @@ resource "aws_key_pair" "default" {
 
   key_name   = var.key_name
   public_key = var.public_key == "" ? file(var.key_path) : var.public_key
-  tags       = var.Tags
+  tags       = module.labels.tags
 }
