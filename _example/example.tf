93606191450549a4a9cfa9a566166fb2a8554a4a
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-01 20:08:24 +0530

    update

--- _example/example.tf
+++ _example/example.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "keypair" {
   source = "./../"
 
-  key_path        = "~/.ssh/id_rsa.pub"
+  key_path        = "/home/runner/work/terraform-aws-keypair/terraform-aws-keypair/id_rsa.pub" # You have to put '~/.ssh/id_rsa.pub' here
   key_name        = "devops"
   enable_key_pair = true
   environment     = "test"
