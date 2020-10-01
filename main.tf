57d576ee76f4dc1fc018ca1045771a20b137b29f
himanshu-uniyal <himanshu@clouddrove.com>
2020-10-01 17:47:01 +0530

    added labels

--- main.tf
+++ main.tf
@@ -4,6 +4,20 @@
 
 #Module      : KEY PAIR
 #Description : Terraform module for generating or importing an SSH public key file into AWS.
+
+module "labels" {
+  source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.13.0"
+
+  name        = var.name
+  application = var.application
+  environment = var.environment
+  label_order = var.label_order
+  managedby   = var.managedby
+  enabled     = var.enable_key_pair
+}
+
+
+
 resource "aws_key_pair" "default" {
   count = var.enable_key_pair == true ? 1 : 0
 
--- varaibles.tf
+++ varaibles.tf
@@ -23,8 +23,32 @@ variable "enable_key_pair" {
   description = "A boolean flag to enable/disable key pair."
 
 }
-variable "Tags" {
-  type        = map(string)
-  default     = { "FirstKey" : "Env", "SecondKey" : "Prod" }
-  description = "A tag is a custom label that you assign to an AWS resource. You can use tags to help organize and identify your key pairs."
+
+variable "environment" {
+  type        = string
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "label_order" {
+  type        = list
+  default     = []
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "application" {
+  type        = string
+  default     = ""
+  description = "Application (e.g. `cd` or `clouddrove`)."
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "anmol@clouddrove.com"
+  description = "ManagedBy, eg 'CloudDrove' or 'AnmolNagpal'."
 }
