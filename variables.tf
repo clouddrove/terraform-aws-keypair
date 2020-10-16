a31ef92db9ca832c5e238570283e9a1ca1f25151
Anmol Nagpal <anmol@clouddrove.com>
2020-10-16 21:59:56 +0530

    update file name

--- variables.tf
+++ variables.tf
@@ -23,3 +23,32 @@ variable "enable_key_pair" {
   description = "A boolean flag to enable/disable key pair."
 
 }
+
+variable "environment" {
+  type        = string
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "label_order" {
+  type        = list
+  default     = []
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "application" {
+  type        = string
+  default     = ""
+  description = "Application (e.g. `cd` or `clouddrove`)."
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "anmol@clouddrove.com"
+  description = "ManagedBy, eg 'CloudDrove' or 'AnmolNagpal'."
+}
