ce7bec0d0c68c7056ef4d50a61b438ad76bae18a
theprashantyadav <79893616+theprashantyadav@users.noreply.github.com>
2023-10-24 13:45:45 +0530

    feat: updated .gitingore and fixed tflint issue (#27)

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 #Module      : KEY PAIR
 #Description : Terraform module key pair outputs.
 output "name" {
-  value       = join("", aws_key_pair.default.*.key_name)
+  value       = join("", aws_key_pair.default[*].key_name)
   description = "Name of SSH key."
 }
 
 output "id" {
-  value       = join("", aws_key_pair.default.*.id)
+  value       = join("", aws_key_pair.default[*].id)
   description = "The key pair name."
 }
 
 output "arn" {
-  value       = join("", aws_key_pair.default.*.arn)
+  value       = join("", aws_key_pair.default[*].arn)
   description = "The key pair ARN."
 }
 output "private_key_id" {
-  value       = try(tls_private_key.default[0].*.id, "")
+  value       = try(tls_private_key.default[0][*].id, "")
   description = "Unique identifier for this resource: hexadecimal representation of the SHA1 checksum of the resource"
 }
 
